2ff2a6ac107ab8d7ae91548849573166bb24971a
Sasi Patterson <sweetpea_th@hotmail.com>
2022-09-12 11:23:16 -0400

    heroku message

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3001;
+
+app.get('/', (req, res) => {
+  res.send({ fun: true });
+});
+
+app.listen(PORT, () => {
+  console.log('app running at http://localhost:' + PORT);
+})
